b22e2b8d1a6703c20cd91918ddcca007263d4d90
AlNomanCSE <abdullahalnomancse@gmail.com>
2024-11-26 07:13:57 -0800

    added Sorting by ID and order

--- src/main/java/org/noman/ecommerce/controller/CategoryController.java
+++ src/main/java/org/noman/ecommerce/controller/CategoryController.java
@@ -26,9 +26,12 @@ public class CategoryController {
 
     @GetMapping("/public/categories")
     public ResponseEntity<CategoryResponse> getAllCategorise(@RequestParam(name = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
-                                                             @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize)
+                                                             @RequestParam(name = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+                                                             @RequestParam(name="sortBy", defaultValue = AppConstants.SORT_CATEGORY_BY, required = false) String sortBy,
+                                                             @RequestParam(name="sortOrder", defaultValue = AppConstants.SORT_DIRECTION, required = false) String sortOrder
+                                                             )
     {
-        CategoryResponse allCategories = categoryService.getAllCategories(pageNumber, pageSize);
+        CategoryResponse allCategories = categoryService.getAllCategories(pageNumber, pageSize,sortBy,sortOrder);
         return new ResponseEntity<>(allCategories, HttpStatus.OK);
     }
 
